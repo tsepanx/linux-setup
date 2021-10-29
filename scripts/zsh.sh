162eaf6dfefc095c2dfd85ec793f52ed8dd36e2e
tsepanx <77626319+tsepanx@users.noreply.github.com>
2021-10-29 14:16:10 +0300

    mv -i

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,8 +1,8 @@
 # yay -S --needed --noconfirm zsh zsh-syntax-highlighting zsh-autosuggestions 
 yay -S --needed --noconfirm zsh
 
-mv .zshrc .zshrc-backup
-mv .zsh .zsh-backup
+mv -i .zshrc .zshrc-backup
+mv -i .zsh .zsh-backup
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.zsh/custom}/plugins/zsh-syntax-highlighting
