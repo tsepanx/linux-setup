df73a5a3be7286dc1044905383be5494d184a964
tsepanx <77626319+tsepanx@users.noreply.github.com>
2021-10-29 14:42:30 +0300

    zsh.sh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,5 +1,8 @@
 # yay -S --needed --noconfirm zsh zsh-syntax-highlighting zsh-autosuggestions 
-yay -S --needed --noconfirm zsh
+[[ $(command -v pacman) ]] && sudo pacman -S --needed --noconfirm zsh
+[[ $(command -v apt) ]] && sudo apt install -y zsh
+
+cd $HOME
 
 mv -i .zshrc .zshrc-backup
 mv -i .zsh .zsh-backup
@@ -8,6 +11,10 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.zsh/custom}/plugins/zsh-autosuggestions
 
-curl https://raw.githubusercontent.com/tsepanx/linux-setup/master/zshrc -o $HOME/.zshrc
+for f in .zshrc .alias_bash .alias_zsh .vars
+
+do
+    [[ ! -f $f ]] && curl https://raw.githubusercontent.com/tsepanx/dotfiles/master/$f -o $HOME/$f
+done
 
 chsh -s /usr/bin/zsh
